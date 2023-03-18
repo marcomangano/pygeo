5e7e6064e8b0d0c654eb61137c6adceec6663341
Marco Mangano <mmangano@umich.edu>
2023-03-18 15:16:29 -0400

    clarify margins docstring

--- pygeo/geo_utils/ffd_generation.py
+++ pygeo/geo_utils/ffd_generation.py
@@ -194,11 +194,13 @@ def createFittedWingFFD(surf, surfFormat, outFile, leList, teList, nSpan, nChord
         List with 3 items specifying the absolute margins in the [chordwise, spanwise, thickness] directions.
         This is useful for areas where the relative margin is too small, such as the trailing edge or wing tip.
         The total margin is the sum of the absolute and relative margins.
+        Margins are applied to all control points in the thickness axis, and only the first and last on the chordwise and spanwise axis.
 
     relMargins : list of float
         List with 3 items specifying the relative margins in the [chordwise, spanwise, thickness] directions.
         Relative margins are applied as a fraction of local chord, wing span, and local thickness.
         The total margin is the sum of the absolute and relative margins.
+        Margins are applied to all control points in the thickness axis, and only the first and last on the chordwise and spanwise axis.
 
     liftIndex : int
         Index specifying which direction lift is in (same as the ADflow option).
